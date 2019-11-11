73efd8f1cec4b19079c6fa9f283e8fc47f2f5332
Yuta Fujii <yutayutakid21@gmail.com>
2019-11-11 18:59:44 +0900

    課題

--- TeamProject/ViewController.swift
+++ TeamProject/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //中塚さん  画面1を作って、入力画面を3つ、次の画面へ渡すプログラム
+    
+    //近藤さん 画面に2を作って、値を取得し、ラベル3つに文字を表示
+    
+    //注意点 ViewController.swiftが中塚さん、NextViewController.swiftが近藤さん
+    
+    //Idはnext
+    
+    //渡す箱はdata1,data2,data3
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
